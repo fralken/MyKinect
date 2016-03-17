e852f566881ca1696127c52db241bddcb544a182
Francesco MDE <francesco.montecuccoli@gmail.com>
2016-03-17 01:39:43 +0100

    implemented green screen and hand tracking

--- MyKinect/KinectManager.cs
+++ MyKinect/KinectManager.cs
@@ -1,19 +1,14 @@
-﻿using Microsoft.Kinect;
+﻿using LightBuzz.Vitruvius;
+using Microsoft.Kinect;
 using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace MyKinect
 {
     class KinectManager
     {
-        /// <summary>
-        /// Size of the RGB pixel in the bitmap
-        /// </summary>
-        private readonly int _bytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
-
         /// <summary>
         /// Active Kinect sensor
         /// </summary>
@@ -32,7 +27,7 @@ namespace MyKinect
         /// <summary>
         /// Bitmap to display
         /// </summary>
-        private WriteableBitmap _bitmap = null;
+        private GreenScreenBitmapGenerator _bitmap = null;
 
         /// <summary>
         /// Drawing group for body rendering output
@@ -43,41 +38,6 @@ namespace MyKinect
         /// Drawing image that we will display
         /// </summary>
         private DrawingImage _drawImage;
-
-        /// <summary>
-        /// The size in bytes of the bitmap back buffer
-        /// </summary>
-        private uint _bitmapBackBufferSize = 0;
-
-        /// <summary>
-        /// Width of display (depth space)
-        /// </summary>
-        private int _depthWidth;
-
-        /// <summary>
-        /// Height of display (depth space)
-        /// </summary>
-        private int _depthHeight;
-
-        /// <summary>
-        /// Width of display (color space)
-        /// </summary>
-        private int _colorWidth;
-
-        /// <summary>
-        /// Height of display (color space)
-        /// </summary>
-        private int _colorHeight;
-
-        /// <summary>
-        /// Intermediate storage for the color to depth mapping
-        /// </summary>
-        private DepthSpacePoint[] _colorMappedToDepthPoints = null;
-
-        /// <summary>
-        /// Array for the bodies
-        /// </summary>
-        private Body[] _bodies = null;
 
         /// <summary>
         /// Constant for clamping Z values of camera space points from being negative
@@ -109,8 +69,9 @@ namespace MyKinect
         /// </summary>
         private const double _ClipBoundsThickness = 10;
 
-        private EventHandler<EventArgs> _guestHandler = null;
-             
+        public EventHandler<EventArgs> GuestHandlerBefore = null;
+        public EventHandler<EventArgs> GuestHandlerAfter = null;
+
         public KinectManager()
         {
             _kinectSensor = KinectSensor.GetDefault();
@@ -121,29 +82,7 @@ namespace MyKinect
 
             _coordinateMapper = _kinectSensor.CoordinateMapper;
 
-            FrameDescription depthFrameDescription = _kinectSensor.DepthFrameSource.FrameDescription;
-
-            _depthWidth = depthFrameDescription.Width;
-            _depthHeight = depthFrameDescription.Height;
-
-            FrameDescription colorFrameDescription = _kinectSensor.ColorFrameSource.FrameDescription;
-
-            _colorWidth = colorFrameDescription.Width;
-            _colorHeight = colorFrameDescription.Height;
-
-            _colorMappedToDepthPoints = new DepthSpacePoint[_colorWidth * _colorHeight];
-
-            InitializeBitmaps();
-
-            _kinectSensor.Open();
-        }
-
-        private void InitializeBitmaps()
-        {
-            double dpiX = 96.0;
-            double dpiY = 96.0;
-
-            _bitmap = new WriteableBitmap(_colorWidth, _colorHeight, dpiX, dpiY, PixelFormats.Bgra32, null);
+            _bitmap = GreenScreenBitmapGenerator.Create(_coordinateMapper, false);
 
             // Create the drawing group we'll use for drawing
             _drawingGroup = new DrawingGroup();
@@ -151,30 +90,17 @@ namespace MyKinect
             // Create an image source that we can use in our image control
             _drawImage = new DrawingImage(_drawingGroup);
 
-            System.Console.WriteLine("*** Depth image pixel width {0:N} pixel height {1:N}", _depthWidth, _depthHeight);
-            System.Console.WriteLine("*** Color image pixel width {0:N} pixel height {1:N} dpiX {2} dpiY {3}", _colorWidth, _colorHeight, dpiX, dpiY);
-            System.Console.WriteLine("*** Bitmap pixel width {0:N} height {1:N}", _bitmap.PixelWidth, _bitmap.PixelHeight);
-            System.Console.WriteLine("*** Bitmap image width {0:N} height {1:N}", _bitmap.Width, _bitmap.Height);
-            System.Console.WriteLine("*** Draw image width {0:N} height {1:N}", _drawImage.Width, _drawImage.Height);
-
-            // Calculate the WriteableBitmap back buffer size
-            _bitmapBackBufferSize = (uint)((_bitmap.BackBufferStride * (_bitmap.PixelHeight - 1)) + (_bitmap.PixelWidth * _bytesPerPixel));
-            System.Console.WriteLine("*** _bitmapBackBufferSize {0}", _bitmapBackBufferSize);
+            _kinectSensor.Open();
         }
 
         public ImageSource getBitmap()
         {
-            return _bitmap;
+            return _bitmap.Bitmap;
         }
 
         public ImageSource getDrawImage()
         {
             return _drawImage;
-        }
-
-        public void add(EventHandler<EventArgs> e)
-        {
-            _guestHandler = e;
         }
 
         public void Dispose()
@@ -201,17 +127,8 @@ namespace MyKinect
         /// <param name="e">event arguments</param>
         private void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
         {
-            if (_guestHandler != null)
-                _guestHandler(sender, e);
-
-            int depthWidth = 0;
-            int depthHeight = 0;
-
-            DepthFrame depthFrame = null;
-            ColorFrame colorFrame = null;
-            BodyIndexFrame bodyIndexFrame = null;
-            BodyFrame bodyFrame = null;
-            bool isBitmapLocked = false;
+            if (GuestHandlerBefore != null)
+                GuestHandlerBefore(sender, e);
 
             MultiSourceFrame multiSourceFrame = e.FrameReference.AcquireFrame();
 
@@ -221,14 +138,11 @@ namespace MyKinect
                 return;
             }
 
-            // We use a try/finally to ensure that we clean up before we exit the function.  
-            // This includes calling Dispose on any Frame objects that we may have and unlocking the bitmap back buffer.
-            try
+            using (var depthFrame = multiSourceFrame.DepthFrameReference.AcquireFrame())
+            using (var colorFrame = multiSourceFrame.ColorFrameReference.AcquireFrame())
+            using (var bodyIndexFrame = multiSourceFrame.BodyIndexFrameReference.AcquireFrame())
+            using (var bodyFrame = multiSourceFrame.BodyFrameReference.AcquireFrame())
             {
-                depthFrame = multiSourceFrame.DepthFrameReference.AcquireFrame();
-                colorFrame = multiSourceFrame.ColorFrameReference.AcquireFrame();
-                bodyIndexFrame = multiSourceFrame.BodyIndexFrameReference.AcquireFrame();
-                bodyFrame = multiSourceFrame.BodyFrameReference.AcquireFrame();
 
                 // If any frame has expired by the time we process this event, return.
                 // The "finally" statement will Dispose any that are not null.
@@ -237,114 +151,18 @@ namespace MyKinect
                     return;
                 }
 
-                // Process Depth
-                FrameDescription depthFrameDescription = depthFrame.FrameDescription;
-
-                depthWidth = depthFrameDescription.Width;
-                depthHeight = depthFrameDescription.Height;
-
-                // Access the depth frame data directly via LockImageBuffer to avoid making a copy
-                using (KinectBuffer depthFrameData = depthFrame.LockImageBuffer())
-                {
-                    _coordinateMapper.MapColorFrameToDepthSpaceUsingIntPtr(
-                        depthFrameData.UnderlyingBuffer,
-                        depthFrameData.Size,
-                        _colorMappedToDepthPoints);
-                }
-
-                // We're done with the DepthFrame 
-                depthFrame.Dispose();
-                depthFrame = null;
-
-                // Process Color
-
-                // Lock the bitmap for writing
-                _bitmap.Lock();
-                isBitmapLocked = true;
-
-                if (colorFrame.RawColorImageFormat == ColorImageFormat.Bgra)
-                {
-                    colorFrame.CopyRawFrameDataToIntPtr(_bitmap.BackBuffer, _bitmapBackBufferSize);
-                }
-                else
-                {
-                    colorFrame.CopyConvertedFrameDataToIntPtr(_bitmap.BackBuffer, _bitmapBackBufferSize, ColorImageFormat.Bgra);
-                }
-
-                // We're done with the ColorFrame 
-                colorFrame.Dispose();
-                colorFrame = null;
-
-                // We'll access the body index data directly to avoid a copy
-                using (KinectBuffer bodyIndexData = bodyIndexFrame.LockImageBuffer())
-                {
-                    unsafe
-                    {
-                        byte* bodyIndexDataPointer = (byte*)bodyIndexData.UnderlyingBuffer;
-
-                        int colorMappedToDepthPointCount = _colorMappedToDepthPoints.Length;
-
-                        fixed (DepthSpacePoint* colorMappedToDepthPointsPointer = _colorMappedToDepthPoints)
-                        {
-                            // Treat the color data as 4-byte pixels
-                            uint* bitmapPixelsPointer = (uint*)_bitmap.BackBuffer;
-
-                            // Loop over each row and column of the color image
-                            // Zero out any pixels that don't correspond to a body index
-                            for (int colorIndex = 0; colorIndex < colorMappedToDepthPointCount; ++colorIndex)
-                            {
-                                float colorMappedToDepthX = colorMappedToDepthPointsPointer[colorIndex].X;
-                                float colorMappedToDepthY = colorMappedToDepthPointsPointer[colorIndex].Y;
-
-                                // The sentinel value is -inf, -inf, meaning that no depth pixel corresponds to this color pixel.
-                                if (!float.IsNegativeInfinity(colorMappedToDepthX) &&
-                                    !float.IsNegativeInfinity(colorMappedToDepthY))
-                                {
-                                    // Make sure the depth pixel maps to a valid point in color space
-                                    int depthX = (int)(colorMappedToDepthX + 0.5f);
-                                    int depthY = (int)(colorMappedToDepthY + 0.5f);
-
-                                    // If the point is not valid, there is no body index there.
-                                    if ((depthX >= 0) && (depthX < depthWidth) && (depthY >= 0) && (depthY < depthHeight))
-                                    {
-                                        int depthIndex = (depthY * depthWidth) + depthX;
-
-                                        // If we are tracking a body for the current pixel, do not zero out the pixel
-                                        if (bodyIndexDataPointer[depthIndex] != 0xff)
-                                        {
-                                            continue;
-                                        }
-                                    }
-                                }
-
-                                bitmapPixelsPointer[colorIndex] = 0;
-                            }
-                        }
-
-                        _bitmap.AddDirtyRect(new Int32Rect(0, 0, _bitmap.PixelWidth, _bitmap.PixelHeight));
-                    }
-                }
+                _bitmap.Update(colorFrame, depthFrame, bodyIndexFrame);
 
                 // bodyFrame
-                if (_bodies == null)
-                {
-                    _bodies = new Body[bodyFrame.BodyCount];
-                }
-
-                // The first time GetAndRefreshBodyData is called, Kinect will allocate each Body in the array.
-                // As long as those body objects are not disposed and not set to null in the array,
-                // those body objects will be re-used.
-                bodyFrame.GetAndRefreshBodyData(_bodies);
-                bodyFrame.Dispose();
-                bodyFrame = null;
+                var bodies = bodyFrame.Bodies();
 
                 using (DrawingContext dc = _drawingGroup.Open())
                 {
                     // Draw a transparent background to set the render size
                     //dc.DrawRectangle(Brushes.Transparent, null, new Rect(0.0, 0.0, _depthWidth, _depthHeight));
-                    dc.DrawRectangle(Brushes.Transparent, null, new Rect(0.0, 0.0, _colorWidth, _colorHeight));
+                    dc.DrawRectangle(Brushes.Transparent, null, new Rect(0.0, 0.0, _bitmap.Bitmap.Width, _bitmap.Bitmap.Height));
 
-                    foreach (Body body in _bodies)
+                    foreach (Body body in bodies)
                     {
                         if (body.IsTracked)
                         {
@@ -365,10 +183,16 @@ namespace MyKinect
                                     position.Z = _InferredZPositionClamp;
                                 }
 
-                                //DepthSpacePoint depthSpacePoint = _coordinateMapper.MapCameraPointToDepthSpace(position);
-                                //jointPoints[jointType] = new Point(depthSpacePoint.X, depthSpacePoint.Y);
-                                ColorSpacePoint colorSpacePoint = _coordinateMapper.MapCameraPointToColorSpace(position);
-                                jointPoints[jointType] = new Point(colorSpacePoint.X, colorSpacePoint.Y);
+                                if (_bitmap.isHD)
+                                {
+                                    ColorSpacePoint colorSpacePoint = _coordinateMapper.MapCameraPointToColorSpace(position);
+                                    jointPoints[jointType] = new Point(colorSpacePoint.X, colorSpacePoint.Y);
+                                }
+                                else
+                                {
+                                    DepthSpacePoint depthSpacePoint = _coordinateMapper.MapCameraPointToDepthSpace(position);
+                                    jointPoints[jointType] = new Point(depthSpacePoint.X, depthSpacePoint.Y);
+                                }
                             }
 
                             DrawHand(body.HandLeftState, jointPoints[JointType.HandLeft], dc);
@@ -378,26 +202,12 @@ namespace MyKinect
 
                     // prevent drawing outside of our render area
                     //_drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, _depthWidth, _depthHeight));
-                    _drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, _colorWidth, _colorHeight));
+                    _drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, _bitmap.Bitmap.Width, _bitmap.Bitmap.Height));
                 }
             }
-            finally
-            {
-                if (isBitmapLocked)
-                    _bitmap.Unlock();
 
-                if (depthFrame != null)
-                    depthFrame.Dispose();
-
-                if (colorFrame != null)
-                    colorFrame.Dispose();
-
-                if (bodyIndexFrame != null)
-                    bodyIndexFrame.Dispose();
-
-                if (bodyFrame != null)
-                    bodyFrame.Dispose();
-            }
+            if (GuestHandlerAfter != null)
+                GuestHandlerAfter(sender, e);
         }
 
         /// <summary>
@@ -433,10 +243,8 @@ namespace MyKinect
         {
             FrameEdges clippedEdges = body.ClippedEdges;
 
-            //int width = _depthWidth;
-            //int height = _depthHeight;
-            int width = _colorWidth;
-            int height = _colorHeight;
+            double width = _bitmap.Bitmap.Width;
+            double height = _bitmap.Bitmap.Height;
 
             if (clippedEdges.HasFlag(FrameEdges.Bottom))
             {
---
+++ MyKinect/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MyKinect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Kinect manager
+        /// </summary>
+        private KinectManager _kinect = null;
+        private DateTime _startTime;
+
+        private int _fps = 0;
+        private double _min_fps = double.MaxValue;
+        private double _max_fps = 0.0;
+        private double _elapsed_seconds = 0;
+        private double _total_frames = 0;
+
+        public MainWindow()
+        {
+            _kinect = new KinectManager();
+            _kinect.GuestHandlerBefore = FPS_counter_starter;
+            _kinect.GuestHandlerAfter = UpdateBitmap;
+
+            DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void FPS_counter_starter(object sender, EventArgs e)
+        {
+            _startTime = DateTime.Now;
+            _kinect.GuestHandlerBefore = FPS_counter;
+        }
+
+        private void FPS_counter(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = DateTime.Now - _startTime;
+            if (elapsed.TotalMilliseconds >= 1000)
+            {
+                double current_fps = _fps * 1000 / elapsed.TotalMilliseconds;
+
+                _elapsed_seconds += elapsed.TotalMilliseconds / 1000;
+                _total_frames += _fps;
+                _fps = 0;
+                _startTime = DateTime.Now;
+
+                if (current_fps < _min_fps) _min_fps = current_fps;
+                if (current_fps > _max_fps) _max_fps = current_fps;
+
+                double avg_fps = _total_frames / _elapsed_seconds;
+
+                fps.Text = String.Format("FPS: {0:0.00}  min: {1:0.00}  max: {2:0.00}  avg: {3:0.00}", current_fps, _min_fps, _max_fps, avg_fps);
+            }
+            _fps++;
+        }
+
+        private void UpdateBitmap(object sender, EventArgs e)
+        {
+            if (ImageSource == null)
+                ImageSource = _kinect.getBitmap();
+        }
+
+        /// <summary>
+        /// The image to display.
+        /// </summary>
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+        /// <summary>
+        /// The <see cref="ImageSource"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(MainWindow), new FrameworkPropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the bitmap to display
+        /// </summary>
+        public ImageSource DrawImage
+        {
+            get
+            {
+                //return this.bitmap;
+                return _kinect.getDrawImage();
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (_kinect != null)
+            {
+                Console.WriteLine("*** Dispose Kinect");
+                _kinect.Dispose();
+                _kinect = null;
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("*** Window Loaded");
+            this.MaximizeToSecondaryMonitor();
+        }
+    }
+
+    static public class WindowExt
+    {
+        static Point RealPixelsToWpf(Window w, Point p)
+        {
+            var t = PresentationSource.FromVisual(w).CompositionTarget.TransformFromDevice;
+            return t.Transform(p);
+        }
+
+        // NB : Best to call this function from the windows Loaded event or after showing the window
+        // (otherwise window is just positioned to fill the secondary monitor rather than being maximised).
+        public static void MaximizeToSecondaryMonitor(this Window window)
+        {
+            var secondaryScreen = System.Windows.Forms.Screen.AllScreens.Where(s => !s.Primary).FirstOrDefault();
+
+            Console.WriteLine("*** Screens: {0}", System.Windows.Forms.Screen.AllScreens.Count());
+            Console.WriteLine("*** WorkingArea: {0}, {1}, {2}, {3}", window.Left, window.Top, window.Width, window.Height);
+            if (secondaryScreen != null)
+            {
+                if (!window.IsLoaded)
+                    window.WindowStartupLocation = WindowStartupLocation.Manual;
+
+                var workingArea = secondaryScreen.WorkingArea;
+                var p1 = RealPixelsToWpf(window, new Point(workingArea.Left, workingArea.Top));
+                //var p2 = RealPixelsToWpf(window, new Point(workingArea.Width, workingArea.Height));
+                window.Left = p1.X;
+                window.Top = p1.Y;
+                //window.Width = p2.X;
+                //window.Height = p2.Y;
+
+                Console.WriteLine("*** WorkingArea: {0}, {1}, {2}, {3}", window.Left, window.Top, window.Width, window.Height);
+            }
+
+            // If window isn't loaded then maxmizing will result in the window displaying on the primary monitor
+            if (window.IsLoaded)
+                window.WindowState = WindowState.Maximized;
+            Console.WriteLine("*** WorkingArea: {0}, {1}, {2}, {3}", window.Left, window.Top, window.Width, window.Height);
+        }
+    }
+}
